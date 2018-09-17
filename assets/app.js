fff14e20322b6269a143282b36fb09991c255774
lance logan <lancelogan@mac.com>
2018-09-17 13:53:27 -0500

    initial design and layout

---
+++ assets/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyDjd8ayPxsiQtqZwIbV8yu8z-Z6mSYzyjE",
+        authDomain: "train-schedule-867a3.firebaseapp.com",
+        databaseURL: "https://train-schedule-867a3.firebaseio.com",
+        projectId: "train-schedule-867a3",
+        storageBucket: "",
+        messagingSenderId: "180988002303"
+      };
+      firebase.initializeApp(config);
+
+      let database = firebase.database();
+
+      //Variables 
+//***********************************
+
+
+
+
+
+
+
+
+
+
+}) //doc ready
